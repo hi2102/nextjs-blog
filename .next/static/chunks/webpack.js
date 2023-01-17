92facc7f1f7f5ae6db1883bcefa066523027fb90
hi2102 <hi2102@naver.com>
2023-01-17 10:55:20 +0900

    build nextJs app

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac9e95a81d7dce0d"; }
+/******/ 		__webpack_require__.h = function() { return "36c7e5e9508d570a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
